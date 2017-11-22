512b302fb323d4e7d90612448c8f060b85753474
Irina Krasnova <sweetdream779@gmail.com>
2017-11-22 22:59:02 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+app = Flask(__name__)
 def wsgi_app(environ, start_response):
     status = '200 OK'
     response_headers = [('Content-type', 'text/plain')]
@@ -7,7 +8,8 @@ def wsgi_app(environ, start_response):
 
 if __name__ == '__main__':
     from wsgiref.simple_server import make_server
-    from flask import Flask
+    from flask import Flask, request, render_template, send_from_directory
 
-    httpd = make_server('localhost', 5555, wsgi_app)
-    httpd.serve_forever()
+    #httpd = make_server('localhost', 5555, wsgi_app)
+    #httpd.serve_forever()
+    app.run(port=80)
