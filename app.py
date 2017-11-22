f3a921240c5071fc54bfcd9dd3824312bd41687a
Irina Krasnova <sweetdream779@gmail.com>
2017-11-22 23:03:38 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ def wsgi_app(environ, start_response):
     status = '200 OK'
     response_headers = [('Content-type', 'text/plain')]
     start_response(status, response_headers)
-    response_body = 'index.html'
+    response_body = 'Hello world'
     yield response_body.encode()
 
 if __name__ == '__main__':
