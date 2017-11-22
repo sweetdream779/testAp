e5ad5cb0491b967533759775bc4cd2225217fc2c
Irina Krasnova <sweetdream779@gmail.com>
2017-11-22 23:02:21 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,13 +3,13 @@ def wsgi_app(environ, start_response):
     status = '200 OK'
     response_headers = [('Content-type', 'text/plain')]
     start_response(status, response_headers)
-    response_body = 'Hello World'
+    response_body = 'index.html'
     yield response_body.encode()
 
 if __name__ == '__main__':
     from wsgiref.simple_server import make_server
     from flask import Flask, request, render_template, send_from_directory
 
-    #httpd = make_server('localhost', 5555, wsgi_app)
-    #httpd.serve_forever()
-    app.run(port=80)
+    httpd = make_server('localhost', 5555, wsgi_app)
+    httpd.serve_forever()
+    #app.run(port=80)
